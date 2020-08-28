06c03f7ab5c4326550c9a0cf1bcac518ff283fd9
IgorBezusenko <bezusenko.98@mail.ru>
2020-08-28 17:00:53 +0300

    Created a Counter and it works

--- src/components/pages/count.js
+++ src/components/pages/count.js
@@ -1,14 +1,48 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
+  onIncClick = () => {
+    let correntCount = this.state.count;
+    this.setState({
+      count: correntCount + 1,
+    });
+  };
+
+  onDecClick = () => {
+    let correntCount = this.state.count;
+    this.setState({
+      count: correntCount - 1,
+    });
+  };
+
+  onResetClick = () => {
+    let correntCount = this.state.count;
+    this.setState({
+      count: correntCount * 0,
+    });
+  };
+
   render() {
     return (
-      <div style={{ width: "400px" }} className="counter">
-        <h1 className="ml-5">0</h1>
-        <div className="d-flex">
-          <button className="btn btn-primary mr-2 ml-2"> + </button>
-          <button className="btn btn-primary mr-2"> - </button>
-          <button className="btn btn-primary"> Reset </button>
+      <div style={{ width: "400px" }} className="container">
+        <h1 className="">Счетчик {this.state.count}</h1>
+        <div className="button-group d-flex">
+          <button
+            onClick={this.onIncClick}
+            className="btn btn-primary mr-2 ml-3"
+          >
+            INC
+          </button>
+          <button onClick={this.onDecClick} className="btn btn-primary mr-2">
+            DEC
+          </button>
+          <button onClick={this.onResetClick} className="btn btn-primary">
+            RESET
+          </button>
         </div>
       </div>
     );
