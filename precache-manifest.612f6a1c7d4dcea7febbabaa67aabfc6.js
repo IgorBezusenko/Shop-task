a870cbb2eb82658b6d728b8cfc3b95c77c08c9d2
IgorBezusenko <bezusenko.98@mail.ru>
2023-11-03 18:39:22 +0200

    Updates

---
+++ precache-manifest.612f6a1c7d4dcea7febbabaa67aabfc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fca9801027a3d7508e04aee7b3a58fb",
+    "url": "/Shop-task/index.html"
+  },
+  {
+    "revision": "d3435a411c296b651160",
+    "url": "/Shop-task/static/css/main.a0f839f8.chunk.css"
+  },
+  {
+    "revision": "98ea7cd022b2f778c1d0",
+    "url": "/Shop-task/static/js/2.3bdcec97.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Shop-task/static/js/2.3bdcec97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3435a411c296b651160",
+    "url": "/Shop-task/static/js/main.18d23fd0.chunk.js"
+  },
+  {
+    "revision": "265a2d4bcb2e1019f64f",
+    "url": "/Shop-task/static/js/runtime-main.904d8410.js"
+  }
+]);
